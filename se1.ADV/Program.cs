f8dca3680c20f21ddc648c328a1db64bb4f593a0
Nadahussein-collab <husseinnada66@gmail.com>
2026-08-30 12:57:51 +0300

    Q1

--- se1.ADV/Program.cs
+++ se1.ADV/Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         {
             #region Q1
-
+            /*1)A generic class:is a class designed with a placeholder for the data type it operates on,
+             * rather than being bound to a specific type. You define it using a type parameter (commonly <T>),
+             * allowing the actual data type to be specified when the class is instantiated.
+             * Using generics : type safety, code reusability, better performance and cleaner code.
+             */
             #endregion
         }
     }
